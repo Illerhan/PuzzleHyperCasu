d4a8cc33f3382c27cc828871046d6b3f32d85b44
Pediluv <119406091+Pediluv@users.noreply.github.com>
2025-01-28 17:56:03 +0100

    LevelMapNumberLevel

---
+++ My project/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem.HID;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject LevelsContainers;
+    private List<GameObject> buttonList;
+
+    
+    private void Start()
+    {
+        buttonList = new List<GameObject>();
+      
+        int count = LevelsContainers.transform.childCount;
+        for (int i = 0; i < count; i++)
+        {
+            buttonList.Add(LevelsContainers.transform.GetChild(i).gameObject.transform.GetChild(0).gameObject);
+            buttonList[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = (i+1).ToString();
+
+        }
+      
+     
+
+
+
+
+    }
+}
