8ab57164d40afe510a156a8537c639ce8475adc9
Pediluv <119406091+Pediluv@users.noreply.github.com>
2025-01-30 12:26:07 +0100

    Bouton associer à chaque niveau

--- My project/Assets/Scripts/LevelManager.cs
+++ My project/Assets/Scripts/LevelManager.cs
@@ -4,34 +4,41 @@ using System.Net.Mime;
 using NUnit.Framework;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem.HID;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject LevelsContainers;
+    public GameObject buttonLevelsContainers;
     private List<GameObject> buttonList;
+    public List<ScriptableObject> levels;
 
     
     private void Start()
     {
         buttonList = new List<GameObject>();
+        levels = new List<ScriptableObject>();
       
-        int count = LevelsContainers.transform.childCount;
+        int count = buttonLevelsContainers.transform.childCount;
         for (int i = 0; i < count; i++)
         {
-            GameObject child = LevelsContainers.transform.GetChild(i).gameObject.transform.GetChild(0).gameObject;
+            GameObject child = buttonLevelsContainers.transform.GetChild(i).gameObject.transform.GetChild(0).gameObject;
             buttonList.Add(child);
             buttonList[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = (i+1).ToString();
-            
+            buttonList[i].name = (i + 1).ToString();
+
 
         }
-      
-     
+        
+    }
 
-
-
+    public void StartLevel()
+    {
+        int index = int.Parse(EventSystem.current.currentSelectedGameObject.name);
+        Debug.Log("Load Level " + index);
 
     }
 }
+
