60d993b2330cbe7753bd21eb3974dd841354ddc6
Alexis Dupire <adupire4@gmail.com>
2025-01-30 16:05:54 +0100

    [merge+add] Convoyeur

--- My project/Assets/Scripts/ConvoyeurFood.cs
+++ My project/Assets/Scripts/ConvoyeurFood.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -20,6 +21,14 @@ public class ConvoyeurFood : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (positions.Length == 0 || positions.Contains(null))
+        {
+            Debug.LogError("Positions array is not properly initialized.");
+            return;
+        }
+        
+        
+   
         foreach (var food in foodList.food)
         {
             itemQueue.Enqueue(food.foodPrefab);
@@ -29,8 +38,12 @@ public class ConvoyeurFood : MonoBehaviour
         {
             SpawnNewFood(i);
         }
+<<<<<<< Updated upstream
         
         //DragableObject.OnItemDropped += HandleItemDropped;
+=======
+
+>>>>>>> Stashed changes
     }
     
     
@@ -69,9 +82,10 @@ public class ConvoyeurFood : MonoBehaviour
 
     void SpawnNewFood(int positionIndex)
     {
-        if (itemQueue.Count == 0) 
+        if (itemQueue.Count <= 0) 
             return;
         DragableObject foodData = itemQueue.Dequeue();
+
         DragableObject newFood = Instantiate(foodData.type.foodPrefab, spawnPoint.position, Quaternion.identity);
         activeItems.Add(newFood);
 
